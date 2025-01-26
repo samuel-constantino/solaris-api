9f821b20c436cdeaf8b3725680a539c675135b8e
Samuel Constantino <samuel.junior@sanp.com.br>
2025-01-26 12:01:59 -0300

    create admin user

--- build.sh
+++ build.sh
@@ -10,3 +10,10 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+# Create a superuser if it doesn't already exist
+python manage.py shell <<EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'hsjkd@shas')
+EOF
