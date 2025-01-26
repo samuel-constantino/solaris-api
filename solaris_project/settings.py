b007f24fe29abc0339e44944ae26113de93bf38c
Samuel Constantino <samuel.junior@sanp.com.br>
2025-01-26 10:50:54 -0300

    adapt api to use django db

--- solaris_project/settings.py
+++ solaris_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,11 +102,12 @@ WSGI_APPLICATION = 'solaris_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost:5432/solaris_db',
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
